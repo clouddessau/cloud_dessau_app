36ccd5d8661102199e751ce25a753b5657a47781
Johannes Jakob <hi@johjakob.com>
2020-10-30 13:53:12 +0100

    feat: Add button and text input components
    Add `ButtonControl` and `TextInputControl` components

---
+++ src/components/controls/ButtonControl.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { TouchableHighlight, View, Text, StyleSheet } from 'react-native'
+import { useTheme } from '@react-navigation/native'
+import { useColorScheme } from 'react-native-appearance'
+import controlColors from '../../styles/controlColors'
+
+const ButtonControl = (props) => {
+  const onPress = (e) => {
+    props.onPress(e)
+  }
+
+  const { colors } = useTheme()
+  const scheme = useColorScheme()
+
+  const styles = StyleSheet.create({
+    container: {
+      borderRadius: 10
+    },
+
+    button: {
+      padding: 14,
+      backgroundColor: controlColors.button[scheme][props.color],
+      borderRadius: 10,
+      alignItems: 'center'
+    },
+
+    buttonText: {
+      color: controlColors.buttonText[scheme][props.color],
+      fontSize: 20
+    }
+  })
+
+  return (
+    <TouchableHighlight onPress={e => onPress(e)} style={styles.container}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.text}</Text>
+      </View>
+    </TouchableHighlight>
+  )
+}
+
+ButtonControl.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(['default', 'blue', 'red'])
+}
+
+ButtonControl.defaultProps = {
+  text: 'Button',
+  color: 'default'
+}
+
+export default ButtonControl
---
+++ src/styles/controlColors.js
@@ -0,0 +1,28 @@
+const controlColors = {
+  button: {
+    light: {
+      default: 'rgb(199, 199, 204)',
+      blue: 'rgb(0, 122, 255)',
+      red: 'rgb(255, 59, 48)'
+    },
+    dark: {
+      default: 'rgb(72, 72, 74)',
+      blue: 'rgb(10, 132, 255)',
+      red: 'rgb(255, 69, 58)'
+    }
+  },
+  buttonText: {
+    light: {
+      default: '#000',
+      blue: '#fff',
+      red: '#fff'
+    },
+    dark: {
+      default: '#fff',
+      blue: '#fff',
+      red: '#fff'
+    }
+  }
+}
+
+export default controlColors
