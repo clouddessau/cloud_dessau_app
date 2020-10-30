2d9a1cd2fc07c5ec8dc5aff37adc94c294fd88d4
Johannes Jakob <hi@johjakob.com>
2020-10-30 13:54:08 +0100

    feat: Add status display and toggle button
    Add [cloud] status display and toggle button to `IndexScreen`

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,37 @@
-import React, { useContext, useLayoutEffect } from 'react'
+import React, { useContext, useLayoutEffect, useEffect, useState } from 'react'
 import { View, Text, Button } from 'react-native'
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useTheme } from '@react-navigation/native'
 import { AuthContext } from '../AuthProvider'
+import ButtonControl from '../components/controls/ButtonControl'
 import firestore from '@react-native-firebase/firestore'
 
 const IndexScreen = ({ navigation }) => {
   const { user, signOut } = useContext(AuthContext)
+  const [openStatus, setOpenStatus] = useState(false)
+  const { colors } = useTheme()
+
+  const onFirestoreResult = (snapshot) => {
+    setOpenStatus(snapshot.get('open'))
+  }
+
+  const onFirestoreError = (error) => {
+    console.log(error)
+  }
+
+  firestore()
+    .collection('status')
+    .doc('cloud_status')
+    .onSnapshot(onFirestoreResult, onFirestoreError)
+
+  const toggleOpenStatus = () => {
+    firestore()
+      .collection('status')
+      .doc('cloud_status')
+      .update({
+        'open': !openStatus
+      })
+  }
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -15,9 +42,16 @@ const IndexScreen = ({ navigation }) => {
   }, [navigation, user]);
 
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, justifyContent: 'center', paddingHorizontal: 16}}>
+      <View>
+        <View>
+          <Text style={{ color: colors.text }}>{openStatus ? "Open!" : "Closed"}</Text>
+        </View>
+        {user &&
+          <ButtonControl onPress={() => toggleOpenStatus()} text={openStatus ? "Close [cloud]" : "Open [cloud]"} color={openStatus ? "red" : "blue"} />
+        }
+      </View>
+    </SafeAreaView>
   )
 }
 
