a4781a23a7ceeed707d75697ba69078a80e0fdd2
Johannes Jakob <hi@johjakob.com>
2020-10-31 13:37:10 +0100

    feat: Add StatusView to IndexScreen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,17 @@
-import React, { useContext, useLayoutEffect, useEffect, useState } from 'react'
-import { View, Text, Button } from 'react-native'
-import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+import React, { useContext, useLayoutEffect, useState } from 'react'
+import { View, Button, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { useTheme } from '@react-navigation/native'
 import { AuthContext } from '../AuthProvider'
+import StatusView from '../components/views/StatusView'
 import ButtonControl from '../components/controls/ButtonControl'
+import styleProperties from '../styles/styleProperties'
 import firestore from '@react-native-firebase/firestore'
 
 const IndexScreen = ({ navigation }) => {
   const { user, signOut } = useContext(AuthContext)
   const [openStatus, setOpenStatus] = useState(false)
+
   const { colors } = useTheme()
 
   const onFirestoreResult = (snapshot) => {
@@ -41,16 +44,20 @@ const IndexScreen = ({ navigation }) => {
     })
   }, [navigation, user]);
 
+  const styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: styleProperties.container.padding,
+      flex: 1,
+      justifyContent: 'space-between'
+    }
+  })
+
   return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center', paddingHorizontal: 16}}>
-      <View>
-        <View>
-          <Text style={{ color: colors.text }}>{openStatus ? "Open!" : "Closed"}</Text>
-        </View>
-        {user &&
-          <ButtonControl onPress={() => toggleOpenStatus()} text={openStatus ? "Close [cloud]" : "Open [cloud]"} color={openStatus ? "red" : "blue"} />
-        }
-      </View>
+    <SafeAreaView style={styles.container}>
+      <StatusView openStatus={openStatus} />
+      {user &&
+        <ButtonControl onPress={() => toggleOpenStatus()} text={openStatus ? "Close [cloud]" : "Open [cloud]"} color={openStatus ? "red" : "blue"} />
+      }
     </SafeAreaView>
   )
 }
