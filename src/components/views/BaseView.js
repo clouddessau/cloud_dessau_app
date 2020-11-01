f801b8e1ed9025781be7f7371ee739f8abd430a4
Johannes Jakob <hi@johjakob.com>
2020-11-01 18:30:04 +0100

    feat: Add BaseView component

---
+++ src/components/views/BaseView.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import styleProperties from '../../styles/styleProperties'
+
+const BaseView = ({ children }) => {
+  const styles = StyleSheet.create({
+    view: {
+      padding: styleProperties.container.padding,
+      flex: 1
+    }
+  })
+
+  return (
+    <SafeAreaView>
+      {children}
+    </SafeAreaView>
+  )
+}
