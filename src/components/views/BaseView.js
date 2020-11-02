b060e0f98255f9161f651e74673e796f92a4b081
Johannes Jakob <hi@johjakob.com>
2020-11-02 15:52:36 +0100

    chore: Rename import

--- src/components/views/BaseView.js
+++ src/components/views/BaseView.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import styleProperties from '../../styles/styleProperties'
+import themeProperties from '../../styles/themeProperties'
 
 const BaseView = ({ children }) => {
   const styles = StyleSheet.create({
     view: {
-      padding: styleProperties.container.padding,
+      padding: themeProperties.container.padding,
       flex: 1
     }
   })
