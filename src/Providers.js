68c1c9c6d11ff0c17695dc01c8e3aa1bdcbe4892
Johannes Jakob <hi@johjakob.com>
2020-11-14 18:03:39 +0100

    feat: Add "react-native-action-sheet" package

--- src/Providers.js
+++ src/Providers.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { AuthProvider } from './AuthProvider'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { AppearanceProvider } from 'react-native-appearance'
+import { ActionSheetProvider } from '@expo/react-native-action-sheet'
 import Routes from './Routes'
 
 const Providers = () => {
@@ -9,7 +10,9 @@ const Providers = () => {
     <AuthProvider>
       <SafeAreaProvider>
         <AppearanceProvider>
-          <Routes />
+          <ActionSheetProvider>
+            <Routes />
+          </ActionSheetProvider>
         </AppearanceProvider>
       </SafeAreaProvider>
     </AuthProvider>
