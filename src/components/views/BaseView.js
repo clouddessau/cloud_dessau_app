41fb00acac615a3539d2ea0e4edc45a112571854
Johannes Jakob <hi@johjakob.com>
2020-11-14 11:33:12 +0100

    update: Add background color to BaseView
    Add background color setting to `BaseView`

--- src/components/views/BaseView.js
+++ src/components/views/BaseView.js
@@ -1,21 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useColorScheme } from 'react-native-appearance'
 import themeProperties from '../../styles/themeProperties'
+import themeColors from '../../styles/themeColors'
 
-const BaseView = ({ children }) => {
+const BaseView = (props) => {
+  const scheme = useColorScheme()
+
   const styles = StyleSheet.create({
     view: {
       padding: themeProperties.container.padding,
+      backgroundColor: themeColors.colors[scheme][props.background],
       flex: 1
     }
   })
 
   return (
     <SafeAreaView style={styles.view}>
-      {children}
+      {props.children}
     </SafeAreaView>
   )
+}
+
+BaseView.propTypes = {
+  background: PropTypes.oneOf(['background', 'backgroundGrouped', 'backgroundSecondary'])
+}
+
+BaseView.defaultProps = {
+  background: 'background'
 }
 
 export default BaseView
